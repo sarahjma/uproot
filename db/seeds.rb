2d703bfde2b3b081ba362afb8f8f0c537e5f9a9e
Sarah Ma <sarahjma4@gmail.com>
2019-12-10 14:27:00 +0100

    DATA: made the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  # enable_extension "plpgsql"
+
+puts "cleaning database"
+Answer.destroy_all
+Question.destroy_all
+QuizResult.destroy_all
+
+puts "creating QuizResult"
+
+  r1 = QuizResult.create
+
+puts "creating Questions"
+
+  q1 = Question.create(
+    category: "mobility",
+    content: "How do you get to work?",
+    display_type: "type-image"
+  )
+
+puts "creating Answers"
+
+  a1 = Answer.create(
+    question: q1,
+    image: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    score: "car",
+    quiz_result: r1
+  )
+
+  a2 = Answer.create(
+    question: q1,
+    image: "https://images.unsplash.com/photo-1507035895480-2b3156c31fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    score: "bike",
+    quiz_result: r1
+  )
+
+  a3 = Answer.create(
+    question: q1,
+    image: "https://images.unsplash.com/photo-1517737812598-1a43d0ef2a60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    score: "train",
+    quiz_result: r1
+  )
+
+  a4 = Answer.create(
+    question: q1,
+    image: "https://images.unsplash.com/photo-1506751470038-e579eb91f580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    score: "walk",
+    quiz_result: r1
+  )
+
+  puts "created #{Answer.count} Answers"
+  puts "created #{QuizResult.count} QuizResults"
+  puts "created #{Question.count} Questions"
+
+
+
+
+
+
+
+
+
