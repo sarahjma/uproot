bdae7d12877a4cdeb68479e8bf262cb060cb380e
Sarah Ma <sarahjma4@gmail.com>
2019-12-10 22:50:57 +0100

    created more api accessors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,261 +7,266 @@
 #   Character.create(name: 'Luke', movie: movies.first)
   # enable_extension "plpgsql"
 
-puts "cleaning database"
-Answer.destroy_all
-Question.destroy_all
-QuizResult.destroy_all
+# puts "cleaning database"
+# Answer.destroy_all
+# Question.destroy_all
+# QuizResult.destroy_all
 
-puts "creating QuizResult"
+# puts "creating QuizResult"
 
-  r1 = QuizResult.create
+#   r1 = QuizResult.create
 
-puts "creating Questions 1"
+# puts "creating Questions 1"
 
-  q1 = Question.create(
-    category: "mobility",
-    content: "How do you get to work?",
-    display_type: "type-image"
-  )
-
-
-  a1 = Answer.create(
-    question: q1,
-    image: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    score: "car"
-  )
-
-  a2 = Answer.create(
-    question: q1,
-    image: "https://images.unsplash.com/photo-1507035895480-2b3156c31fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    score: "bike"
-  )
-
-  a3 = Answer.create(
-    question: q1,
-    image: "https://images.unsplash.com/photo-1517737812598-1a43d0ef2a60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    score: "train"
-  )
-
-  a4 = Answer.create(
-    question: q1,
-    image: "https://images.unsplash.com/photo-1506751470038-e579eb91f580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    score: "walk"
-  )
+#   q1 = Question.create(
+#     category: "mobility",
+#     content: "How do you get to work?",
+#     display_type: "type-image"
+#   )
 
 
-puts "creating Questions 2"
+#   a1 = Answer.create(
+#     question: q1,
+#     image: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+#     score: "car"
+#   )
 
-  q2 = Question.create(
-    category: "career",
-    content: "What do you do for work?",
-    display_type: "type-dropdown"
-  )
+#   a2 = Answer.create(
+#     question: q1,
+#     image: "https://images.unsplash.com/photo-1507035895480-2b3156c31fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+#     score: "bike"
+#   )
 
+#   a3 = Answer.create(
+#     question: q1,
+#     image: "https://images.unsplash.com/photo-1517737812598-1a43d0ef2a60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+#     score: "train"
+#   )
 
-  a1 = Answer.create(
-    question: q2,
-    text: "doctor",
-    score: "doctor",
-  )
-
-  a2 = Answer.create(
-    question: q2,
-    text: "tech",
-    score: "tech",
-  )
-
-  a3 = Answer.create(
-    question: q2,
-    text: "teacher",
-    score: "teacher",
-  )
-
-  a4 = Answer.create(
-    question: q2,
-    text: "artist",
-    score: "artist",
-  )
+#   a4 = Answer.create(
+#     question: q1,
+#     image: "https://images.unsplash.com/photo-1506751470038-e579eb91f580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+#     score: "walk"
+#   )
 
 
-puts "creating Questions 3"
+# puts "creating Questions 2"
 
-  q3 = Question.create(
-    category: "leisure",
-    content: "What do you usually do in your free time?",
-    display_type: "type-buttons"
-  )
-
-
-  a1 = Answer.create(
-    question: q3,
-    text: "gym",
-    score: "gym",
-  )
-
-  a2 = Answer.create(
-    question: q3,
-    text: "museum",
-    score: "museum",
-  )
-
-  a3 = Answer.create(
-    question: q3,
-    text: "cafe",
-    score: "cafe",
-  )
-
-  a4 = Answer.create(
-    question: q3,
-    text: "concerts",
-    score: "concerts",
-  )
-
-puts "creating Questions 4"
-
-  q4 = Question.create(
-    category: "housing",
-    content: "What is home?",
-    display_type: "type-image"
-  )
+#   q2 = Question.create(
+#     category: "career",
+#     content: "What do you do for work?",
+#     display_type: "type-dropdown"
+#   )
 
 
-  a1 = Answer.create(
-    question: q4,
-    image: "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2553&q=80",
-    score: "apartment"
-  )
+#   a1 = Answer.create(
+#     question: q2,
+#     text: "doctor",
+#     score: "doctor",
+#   )
 
-  a2 = Answer.create(
-    question: q4,
-    image: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3906&q=80",
-    score: "house"
-  )
+#   a2 = Answer.create(
+#     question: q2,
+#     text: "tech",
+#     score: "tech",
+#   )
 
-  a3 = Answer.create(
-    question: q4,
-    image: "https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2989&q=80",
-    score: "villa"
-  )
+#   a3 = Answer.create(
+#     question: q2,
+#     text: "teacher",
+#     score: "teacher",
+#   )
 
-  a4 = Answer.create(
-    question: q4,
-    image: "https://images.unsplash.com/photo-1548513830-5e684f0d4d82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2989&q=80",
-    score: "mobile-home"
-  )
-puts "creating Questions 5"
-
-  q5 = Question.create(
-    category: "leisure",
-    content: "It's Friday! What does it look like?",
-    display_type: "type-image"
-  )
+#   a4 = Answer.create(
+#     question: q2,
+#     text: "artist",
+#     score: "artist",
+#   )
 
 
-  a1 = Answer.create(
-    question: q5,
-    image: "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80",
-    score: "homebody"
-  )
+# puts "creating Questions 3"
 
-  a2 = Answer.create(
-    question: q5,
-    image: "https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80",
-    score: "outgoing"
-  )
-
-  a3 = Answer.create(
-    question: q5,
-    image: "https://images.unsplash.com/photo-1543007630-9710e4a00a20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80",
-    score: "outgoing"
-  )
-
-  a4 = Answer.create(
-    question: q5,
-    image: "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-    score: "chill"
-  )
-
-  a5 = Answer.create(
-  question: q5,
-  image: "https://images.unsplash.com/photo-1558086478-d632ccc5a833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2546&q=80",
-  score: "homebody"
-  )
-
-  a6 = Answer.create(
-  question: q5,
-  image: "https://images.unsplash.com/photo-1571942389762-b0502602ed4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1700&q=80",
-  score: "homebody"
-  )
-
-  puts "creating Questions 6"
-
-  q6 = Question.create(
-    category: "nature",
-    content: "It's Saturday. You finally have a free day. Where do you choose to spend it?",
-    display_type: "type-image"
-  )
+#   q3 = Question.create(
+#     category: "leisure",
+#     content: "What do you usually do in your free time?",
+#     display_type: "type-buttons"
+#   )
 
 
-  a1 = Answer.create(
-    question: q6,
-    image: "https://images.unsplash.com/photo-1446844805183-9f5af45f89ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80",
-    score: "park"
-  )
+#   a1 = Answer.create(
+#     question: q3,
+#     text: "gym",
+#     score: "gym",
+#   )
 
-  a2 = Answer.create(
-    question: q6,
-    image: "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-    score: "mountains"
-  )
+#   a2 = Answer.create(
+#     question: q3,
+#     text: "museum",
+#     score: "museum",
+#   )
 
-  a3 = Answer.create(
-    question: q6,
-    image: "https://images.unsplash.com/photo-1496275068113-fff8c90750d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-    score: "beach"
-  )
+#   a3 = Answer.create(
+#     question: q3,
+#     text: "cafe",
+#     score: "cafe",
+#   )
 
-  a4 = Answer.create(
-    question: q6,
-    image: "https://images.unsplash.com/photo-1551524559-8af4e6624178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80",
-    score: "ski"
-  )
+#   a4 = Answer.create(
+#     question: q3,
+#     text: "concerts",
+#     score: "concerts",
+#   )
 
-  a5 = Answer.create(
-  question: q6,
-  image: "https://images.unsplash.com/photo-1481437156560-3205f6a55735?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-  score: "shopping"
-  )
+# puts "creating Questions 4"
 
-  a6 = Answer.create(
-  question: q6,
-  image: "https://images.unsplash.com/photo-1550763200-c150c99abfbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  score: "homebody"
-  )
-
-  puts "creating Questions 7"
-
-  q7 = Question.create(
-    category: "housing",
-    content: "Rent is due. How much do you pay?",
-    display_type: "type-range"
-  )
+#   q4 = Question.create(
+#     category: "housing",
+#     content: "What is home?",
+#     display_type: "type-image"
+#   )
 
 
-  a1 = Answer.create(
-    question: q7,
-    min: 0,
-    max: 2000,
-    score: "rent"
-  )
+#   a1 = Answer.create(
+#     question: q4,
+#     image: "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2553&q=80",
+#     score: "apartment"
+#   )
 
-  puts "created #{Answer.count} Answers"
-  puts "created #{QuizResult.count} QuizResults"
-  puts "created #{Question.count} Questions"
+#   a2 = Answer.create(
+#     question: q4,
+#     image: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3906&q=80",
+#     score: "house"
+#   )
+
+#   a3 = Answer.create(
+#     question: q4,
+#     image: "https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2989&q=80",
+#     score: "villa"
+#   )
+
+#   a4 = Answer.create(
+#     question: q4,
+#     image: "https://images.unsplash.com/photo-1548513830-5e684f0d4d82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2989&q=80",
+#     score: "mobile-home"
+#   )
+# puts "creating Questions 5"
+
+#   q5 = Question.create(
+#     category: "leisure",
+#     content: "It's Friday! What does it look like?",
+#     display_type: "type-image"
+#   )
+
+
+#   a1 = Answer.create(
+#     question: q5,
+#     image: "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80",
+#     score: "homebody"
+#   )
+
+#   a2 = Answer.create(
+#     question: q5,
+#     image: "https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80",
+#     score: "outgoing"
+#   )
+
+#   a3 = Answer.create(
+#     question: q5,
+#     image: "https://images.unsplash.com/photo-1543007630-9710e4a00a20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80",
+#     score: "outgoing"
+#   )
+
+#   a4 = Answer.create(
+#     question: q5,
+#     image: "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+#     score: "chill"
+#   )
+
+#   a5 = Answer.create(
+#   question: q5,
+#   image: "https://images.unsplash.com/photo-1558086478-d632ccc5a833?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2546&q=80",
+#   score: "homebody"
+#   )
+
+#   a6 = Answer.create(
+#   question: q5,
+#   image: "https://images.unsplash.com/photo-1571942389762-b0502602ed4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1700&q=80",
+#   score: "homebody"
+#   )
+
+#   puts "creating Questions 6"
+
+#   q6 = Question.create(
+#     category: "nature",
+#     content: "It's Saturday. You finally have a free day. Where do you choose to spend it?",
+#     display_type: "type-image"
+#   )
+
+
+#   a1 = Answer.create(
+#     question: q6,
+#     image: "https://images.unsplash.com/photo-1446844805183-9f5af45f89ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80",
+#     score: "park"
+#   )
+
+#   a2 = Answer.create(
+#     question: q6,
+#     image: "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+#     score: "mountains"
+#   )
+
+#   a3 = Answer.create(
+#     question: q6,
+#     image: "https://images.unsplash.com/photo-1496275068113-fff8c90750d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+#     score: "beach"
+#   )
+
+#   a4 = Answer.create(
+#     question: q6,
+#     image: "https://images.unsplash.com/photo-1551524559-8af4e6624178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80",
+#     score: "ski"
+#   )
+
+#   a5 = Answer.create(
+#   question: q6,
+#   image: "https://images.unsplash.com/photo-1481437156560-3205f6a55735?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+#   score: "shopping"
+#   )
+
+#   a6 = Answer.create(
+#   question: q6,
+#   image: "https://images.unsplash.com/photo-1550763200-c150c99abfbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+#   score: "homebody"
+#   )
+
+#   puts "creating Questions 7"
+
+#   q7 = Question.create(
+#     category: "housing",
+#     content: "Rent is due. How much do you pay?",
+#     display_type: "type-range"
+#   )
+
+
+#   a1 = Answer.create(
+#     question: q7,
+#     min: 0,
+#     max: 2000,
+#     score: "rent"
+#   )
+
+#   puts "created #{Answer.count} Answers"
+#   puts "created #{QuizResult.count} QuizResults"
+#   puts "created #{Question.count} Questions"
 
 #======================================================
 # Veronica testing API (let's see how this goes)
 # First let's get the cities:
+require 'open-uri'
+require 'json'
+
+
+
 url = 'https://api.teleport.org/api/countries/'
 uri = URI(url)
 response = Net::HTTP.get(uri)
@@ -275,6 +280,8 @@ end
 
 # Do loop for each of the cities:
 # Table: cities, Property: health;
+
+# Health from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:#{city}/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
@@ -282,39 +289,54 @@ response = Net::HTTP.get(uri)
 search = JSON.parse(response)
  puts search['categories'][7]['data'].select{|property| property["id"] == "HEALTHCARE-QUALITY-TELESCORE" }[0]["float_value"]
 
-# Sarah for safety
+# Sarah for safety from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:#{city}/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 search = JSON.parse(response)
 puts search['categories'][16]['data'].select{|property| property["id"] == "CRIME-RATE-TELESCORE"}[0]["float_value"]
 
-# Sarah mobility car
+# Sarah mobility car from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:#{city}/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 search = JSON.parse(response)
 puts search['categories'][19]['data'].select{|property| property["id"] == "TRAFFIC-INDEX-TELESCORE"}[0]["float_value"]
 
-# Average weather high
+# Average weather high from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:amsterdam/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 search = JSON.parse(response)
 puts search['categories'][2]['data'].select{|property| property['id'] == "WEATHER-AVERAGE-HIGH"}[0]["string_value"]
 
-# Average weather low
+# Average weather low from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:amsterdam/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 search = JSON.parse(response)
 puts search['categories'][2]['data'].select{|property| property["id"] == "WEATHER-AVERAGE-LOW"}[0]["string_value"]
 
-# Weather by type
+# Weather by type from teleport
 url = "https://api.teleport.org/api/urban_areas/slug:amsterdam/details/"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 search = JSON.parse(response)
 puts search['categories'][2]['data'].select{|property| property["id"] == "WEATHER-TYPE"}[0]["string_value"]
+
+# Education from teleport
+url = "https://api.teleport.org/api/urban_areas/slug:amsterdam/details/"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+search = JSON.parse(response)
+puts search['categories'][6]['data'].select{|property| property["id"] == "QUALITY-OF-UNIVERSITIES-TELESCORE"}[0]["float_value"]
+
+
+# Housing- rent index score from teleport
+url = "https://api.teleport.org/api/urban_areas/slug:amsterdam/details/"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+search = JSON.parse(response)
+puts search['categories'][8]['data'].select{|property| property["id"] == "RENT-INDEX-TELESCORE"}[0]["float_value"]
 
 
