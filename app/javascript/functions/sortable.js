864c32901d59646f2d35208d4adab1ea6f1e1590
Veronica HW <veronica.wajda@gmail.com>
2019-12-13 16:54:23 +0100

    FEAT: JS sortable styling for mobile

--- app/javascript/functions/sortable.js
+++ app/javascript/functions/sortable.js
@@ -1,8 +1,26 @@
 import { initSortable } from '../plugins/init_sortable'; // <-- add this
 
 const startSortable = () => {
-  if (document.querySelector("#sortable-list")) {
+  const container = document.querySelector("#sortable-list")
+  if (container) {
     initSortable();
+    const listItems = container.querySelectorAll("li")
+    listItems.forEach(listItem => {
+      listItem.addEventListener(('touchstart'), () => {
+        listItem.classList.add('drag')
+      })
+      listItem.addEventListener(('touchmove'), () => {
+        listItem.classList.add('chosen')
+      })
+      listItem.addEventListener(('touchend'), () => {
+        setTimeout(function(){
+          listItem.classList.remove('drag')
+        }, 300)
+      })
+      listItem.addEventListener(('touchend'), () => {
+          listItem.classList.remove('chosen')
+      })
+    })
   }
 }
 
