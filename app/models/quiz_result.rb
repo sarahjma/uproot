2ee22e94abfc8cd32d0df52ef2f32e45c8209a14
Isac Kim <jk724814@gmail.com>
2019-12-16 14:14:08 +0100

    FEAT: added the scor to be equal to the api score

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -9,18 +9,6 @@ class QuizResult < ApplicationRecord
     define_priorities
   end
 
-  def define_priorities
-    weighting = {}
-    values = [0.35, 0.3, 0.2, 0.05, 0.05, 0.05, 0, 0]
-    @chosen_answer_category.each_with_index { |category, index|
-      result[category.to_sym] = values[index] }
-    overall_city_score = Hash.new(0)
-    @cities.each do |city|
-      weighting.each do |category, weight|
-        overall_city_score[category] = logic_category(category, city) * weight
-      end
-    end
-  end
 
   private
 
@@ -33,12 +21,35 @@ class QuizResult < ApplicationRecord
   def logic_category(category, city)
     # Check the category of a question
     quiz_result.chosen_answers.each do |chosen_answer|
-    sum_of_scores_array = []
-    cat = chosen_answer.answer.question.category
-    score = chosen_answer.answer.score
-    sum_of_scores_array << score if cat == category
-    return sum_of_scores_array.sum / sum_of_scores_array.count
+      sum_of_scores_array = []
+      cat = chosen_answer.answer.question.category
+      score = chosen_answer.answer.score
+      total_score = 0
+      temp_min = 0
+      temp_max = 0
+      if score.include?("rent")
+        total_score = city.send("#{score}_price")
+      elsif score.include?("temp")
+        temp_min = city.send("#{score}_min")
+        temp_max = city.send("#{score}_max")
+      else
+        total_score = city.send("#{score}_score")
+      end
+      sum_of_scores_array << score if cat == category
+      return sum_of_scores_array.sum / sum_of_scores_array.count
     end
   end
 
+  def define_priorities
+    weighting = {}
+    values = [0.35, 0.3, 0.2, 0.05, 0.05, 0.05, 0, 0]
+    @chosen_answer_category.each_with_index { |category, index|
+      result[category.to_sym] = values[index] }
+    overall_city_score = Hash.new(0)
+    @cities.each do |city|
+      weighting.each do |category, weight|
+        overall_city_score[category] = logic_category(category, city) * weight
+      end
+    end
+  end
 end
