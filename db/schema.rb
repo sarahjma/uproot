945014684f72c1bdb7bdbb6de623973dd2672fd7
Isac Kim <jk724814@gmail.com>
2019-12-17 16:44:53 +0100

    FEAT: added a search bar and its links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_164531) do
+ActiveRecord::Schema.define(version: 2019_12_17_140419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 2019_12_16_164531) do
     t.integer "concerts_venues"
     t.string "lgbtq_status"
     t.string "career", default: [], array: true
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "questions", force: :cascade do |t|
