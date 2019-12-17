2352951cc7d7d4778fd74684c2e6f2046ae7446b
Veronica HW <veronica.wajda@gmail.com>
2019-12-17 20:11:30 +0100

    WIP: need assistance

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -6,20 +6,8 @@ class QuizResult < ApplicationRecord
   def top_3_cities(sorted_categories)
     top3 = {}
     city_scores = calculate_city_score(sorted_categories)
-    #binding.pry
-
-    top3 = city_scores.sort_by{ | k, v| v }.reverse[0..2]
-    # city_scores.values.sort.reverse[0..2].each_with_index do |city_value, index|
-    #   top3[index + 1] = city_scores.key(city_value)
-    # end
-    return top3.to_h
+    top3 = city_scores.sort_by { |_k, v| v }.reverse[0..2].to_h
   end
-
-  # 1. Group results per the category. (LOGIC_CATEGORY)
-  # 2. Calculate the score of each of the categories. (OVERALL_CATEGORY_SCORES)
-  # 3. Get the sorted order of priority. (CATEGORY_WEIGHTINGS)
-  # 4. Calculate a final score for each city (OVERALL_CITY_SCOREA)
-  # 5. Return the top 3 cities (TOP_3_CITIES)
 
   private
 
@@ -27,48 +15,42 @@ class QuizResult < ApplicationRecord
     quiz_result = QuizResult.last
     sum_scores_array = []
     total_score = 0
-
     temp_min = 0
     temp_max = 0
-
     quiz_result.chosen_answers.each do |chosen_answer|
       question_id = Answer.find(chosen_answer.answer_id).question_id
-      cat = Question.find(question_id).category
+      question_category = Question.find(question_id).category
       score = chosen_answer.answer.score
-
-      # This gives a score its corresponding value
       if score.include?("rent")
         total_score = city.send("#{score}_price")
       elsif score.include?("temp")
+        binding.pry
         temp_min = city.send("#{score}_min")
         temp_max = city.send("#{score}_max")
       else
         total_score = city.send("#{score}_score")
       end
-      sum_scores_array << total_score if cat.to_s == category.to_s
+      sum_scores_array << total_score if question_category.to_s == category.to_s
     end
     sum_scores_array.sum / sum_scores_array.count
   end
 
-  def is_rent_compatible?(city)
-    # get the score of the chosen apartment. find the question with category housing.
+  def rent_compatible?(city)
     question = Question.find_by(category: 'housing')
-    # find the answers belonging to that question
     answers = question.answers
-    # find chosen answers belonging to that question
     chosen_answers = ChosenAnswer.where(
       quiz_result_id: id,
       answer_id: answers
     )
     puts chosen_answers
     apartment_size = chosen_answers.first.answer.score
-    # return true if rent < city.rent
-    # else return false rent_medium_price
-    if apartment_size == "medium_rent"
-      if rent < city.rent_medium_price #rent 500, med 1000
-        return true
-      else
-        return false
+    case apartment_size
+      when "rent_small"
+        rent < city.rent_medium_price
+      when "rent_medium"
+        rent < city.rent_small_price
+      when "rent_large"
+        rent < city.rent_large_price
       end
     end
   end
@@ -87,9 +69,7 @@ class QuizResult < ApplicationRecord
 
     City.all.each do |city|
       overall_city_score = 0
-      # if rent is not possible for city then return zero as overall city score.
-      # if rent is possible, do a weighting loop to get an overall city score.
-      if is_rent_compatible?(city)
+      if rent_compatible?(city)
         weightings.each do |category, weight|
           overall_category_scores[category] = logic_category(category, city) * weight
           # OVERALL_CATEGORY_SCORE returns { category: overal_category_score }
--- test/models/quiz_result_test.rb
+++ test/models/quiz_result_test.rb
@@ -10,18 +10,25 @@ class QuizResultTest < ActiveSupport::TestCase
       City.create(
         name: "Calgary",
         rent_medium_price: 1000,
-        bike_score: 1
+        bike_score: 1,
+        traffic_score: 1,
+        galleries_score: 1
+
       )
       City.create(
         name: "Paris",
         rent_medium_price: 50,
-        bike_score: 1
+        bike_score: 1,
+        traffic_score: 1,
+        galleries_score: 1
       )
     end
+
     it "should return paris if max rent is 500" do
       quiz_result = QuizResult.create(
         rent: 500
       )
+
       housing_question = Question.create(
         category: "housing",
         content: "Which looks most like home?",
@@ -37,53 +44,75 @@ class QuizResultTest < ActiveSupport::TestCase
         quiz_result_id: quiz_result.id
       )
 
-
-      otherresult = quiz_result.top_3_cities(['mobility']).keys.first
+      result = quiz_result.top_3_cities(['mobility'])
       #binding.pry
       puts "rent test"
-      puts otherresult
+      puts result
 
-      assert_equal "Paris", otherresult
+      assert_equal "Paris", result
     end
   end
 
 
-  describe "category test" do
-    before do
-      City.destroy_all
-      Question.destroy_all
+  # describe "category test" do
+  #   before do
+  #     City.destroy_all
+  #     Question.destroy_all
 
-      City.create(
-        name: "Amsterdam",
-        beach_score: 0,
-        bike_score: 0.9
-      )
+  #     City.create(
+  #       name: "Amsterdam",
+  #       beach_score: 0,
+  #       bike_score: 0.9,
+  #       rent_medium_price: 300
+  #     )
 
-      City.create(
-        name: "LA",
-        beach_score: 0.9,
-        bike_score: 0.1
-      )
+  #     City.create(
+  #       name: "LA",
+  #       beach_score: 0.9,
+  #       bike_score: 0.1,
+  #       rent_medium_price: 50,
+  #     )
 
-    end
-    # chosen_answer_category = ["mobility", "education", "housing", "safety", "career", "leisure", "health"]
+  #   end
+  #   # chosen_answer_category = ["mobility", "education", "housing", "safety", "career", "leisure", "health"]
 
-    it "chosen answer ephasis on bike so amsterdam wins" do
-      chosen_answer = ChosenAnswer.create(
-        quiz_result: QuizResult.create,
-          answer: Answer.create(
-            question: Question.create(category: "mobility"),
-            score: "bike"
-          )
-        )
-      chosen_answer_category = ["mobility"]
-      binding.pry
-      result = chosen_answer.quiz_result.top_3_cities(chosen_answer_category)
-      puts "bike test"
-      puts result
+  #   it "chosen answer ephasis on bike so amsterdam wins" do
+  #     quiz_result_housing = QuizResult.create(
+  #       rent: 500
+  #     )
+  #     housing_question = Question.create(
+  #       category: "housing",
+  #       content: "Which looks most like home?",
+  #       display_type: "type-image"
+  #     )
+  #     housing_answer = Answer.create(
+  #       question: housing_question,
+  #       score: "rent_medium_price"
+  #     )
 
-      assert result["Amsterdam"] > result["LA"]
-    end
+  #     chosen_answer_housing = ChosenAnswer.create(
+  #       answer_id: housing_answer.id,
+  #       quiz_result_id: quiz_result_housing.id
+  #     )
+
+  #     # chosen_answer = ChosenAnswer.create(
+
+  #     #   quiz_result: QuizResult.create,
+  #     #   answer: Answer.create(
+  #     #     question: Question.create(category: "mobility"),
+  #     #     score: "bike"
+  #     #   )
+  #     # )
+  #     chosen_answer_category = ["mobility"]
+
+  #     result = chosen_answer_housing.quiz_result.top_3_cities(chosen_answer_category)
+  #     puts result
+
+  #     assert result["Amsterdam"] > result["LA"]
+  #     # assert result[2], "LA"
+  #   end
+  # end
+end
 
       # it "chosen answer emphasis on beach so LA wins" do
       # chosen_answer = ChosenAnswer.create(
@@ -100,6 +129,24 @@ class QuizResultTest < ActiveSupport::TestCase
       # puts result
 
       # assert result["Amsterdam"] > result["LA"]
-    end
-  end
-end
+  #   end
+  # end
+# end
+
+
+# test "chosen answer ephasis on bike so amsterdam wins" do
+#     chosen_answer = ChosenAnswer.create(
+#       quiz_result: QuizResult.create,
+#       answer: Answer.create(
+#         question: Question.create(category: "mobility"),
+#         score: "bike"
+#       )
+#     )
+#     chosen_answer_category = ["mobility"]
+
+#     result = chosen_answer.quiz_result.top_3_cities(chosen_answer_category)
+#     puts result
+
+#     assert result[1], "Amsterdam"
+#     assert result[2], "LA"
+#   end
