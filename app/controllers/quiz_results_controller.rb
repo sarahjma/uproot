f6c09bf89bfa612c731a87ab26e4e2a0b83fdcb0
Sarah Ma <sarahjma4@gmail.com>
2019-12-18 10:09:27 +0100

    made a little logic for what the quiz result show page should be. haven't been able to test it yet

--- app/controllers/quiz_results_controller.rb
+++ app/controllers/quiz_results_controller.rb
@@ -7,11 +7,11 @@ class QuizResultsController < ApplicationController
     redirect_to quiz_result_question_path(quiz_result, Question.first)
   end
 
-  def show
+  # def show
+  #   @cities = @quiz_result.top_3_cities(@chosen_answer_category)
+  # end
+
+  def results
     @cities = @quiz_result.top_3_cities
   end
-
-  # def results
-  #   @cities = @quiz_result.top_3_cities
-  # end
 end
