7c27e5835b490da60678594610fceac9bd107232
Elisa Van der Perre <elisa.vanderperre@hotmail.com>
2019-12-18 15:27:53 +0100

    button fixed

--- app/javascript/functions/submit_btn.js
+++ app/javascript/functions/submit_btn.js
@@ -2,13 +2,13 @@ const submit = () => {
   const labels = document.querySelectorAll(".query")
   const btn_next = document.querySelector(".btn-next")
   if (document.querySelector('.query')){
-    btn_next.hidden = true
+    btn_next.disabled = true
     labels.forEach((label) => {
       label.addEventListener("click", (event) =>{
-        btn_next.hidden = false
+        btn_next.disabled = false
       })
     })
   }
 }
 
-export { submit}
+export { submit }
