467e09019df2f3b7c84155949d800677d7379c90
Isac Kim <jk724814@gmail.com>
2019-12-19 15:57:55 +0100

    FEAT: finished styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_094611) do
-
+ActiveRecord::Schema.define(version: 2019_12_19_125541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
