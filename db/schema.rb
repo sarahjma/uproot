a534e362a0af977000bdffbea9ac93e7955527a3
Elisa Van der Perre <elisa.vanderperre@hotmail.com>
2019-12-19 15:32:21 +0100

    user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_160624) do
+ActiveRecord::Schema.define(version: 2019_12_19_105005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,8 @@ ActiveRecord::Schema.define(version: 2019_12_18_160624) do
     t.datetime "updated_at", null: false
     t.integer "rent"
     t.string "sorted_categories", default: [], array: true
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_quiz_results_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
